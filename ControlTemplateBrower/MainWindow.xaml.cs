f00998581ac3baa4be6de44098e604e2026fce75
YanBingxin <775127481@qq.com>
2017-06-22 16:18:28 +0800

    WPF默认模板浏览器
    创建初版

---
+++ ControlTemplateBrower/MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+using System.Windows.Markup;
+using System.Windows.Annotations;
+
+namespace ControlTemplateBrower
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Type controlType = typeof(Control);
+            List<Type> derivedTypes = new List<Type>();
+
+            Assembly assembly = Assembly.GetAssembly(typeof(Control));
+            foreach (Type type in assembly.GetTypes())
+            {
+                if (type.IsSubclassOf(controlType) && !type.IsAbstract && type.IsPublic)
+                {
+                    derivedTypes.Add(type);
+                }
+            }
+
+            lstTypes.ItemsSource = derivedTypes.OrderBy(t => t.Name);
+        }
+
+        private void lstTypes_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                Type type = (Type)lstTypes.SelectedItem;
+
+                ConstructorInfo info = type.GetConstructor(System.Type.EmptyTypes);
+                Control control = (Control)info.Invoke(null);
+
+                control.Visibility = Visibility.Collapsed;
+                if (type.Name == typeof(ToolTip).Name)
+                {
+                    (control as ToolTip).IsOpen = true;
+                }
+                else if (type.Name == typeof(ContextMenu).Name)
+                {
+                    (control as ContextMenu).IsOpen = true;
+                }
+                else if (type.Name.Contains("Window"))
+                {
+                    (control as Window).Width = 0;
+                    (control as Window).Height = 0;
+                    (control as Window).Show();
+                    (control as Window).Close();
+                }
+                else if (type.Name == "StickyNoteControl")
+                {
+                    //AnnotationService.CreateTextStickyNoteCommand.Execute("xxx",txtTemplate);
+                }
+                else
+                {
+                    grid.Children.Add(control);
+                }
+
+                ControlTemplate template = control.Template;
+
+                XmlWriterSettings settings = new XmlWriterSettings();
+                settings.Indent = true;
+                StringBuilder sb = new StringBuilder();
+                XmlWriter writer = XmlWriter.Create(sb, settings);
+                XamlWriter.Save(template, writer);
+
+                txtTemplate.Text = sb.ToString();
+
+                grid.Children.Remove(control);
+
+
+            }
+            catch (Exception ex)
+            {
+
+                txtTemplate.Text = "<<Error generationg template:" + ex.Message + ">>";
+            }
+        }
+    }
+}
